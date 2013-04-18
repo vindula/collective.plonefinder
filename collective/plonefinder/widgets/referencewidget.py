1bc9b30e65c2c5a21a8f236425ffb160d23f4848
Solgema <martronic@martronic.ch>
2013-04-18 18:22:22 +0300

    Plone 4.3 compatibility

--- collective/plonefinder/widgets/referencewidget.py
+++ collective/plonefinder/widgets/referencewidget.py
@@ -5,7 +5,10 @@
 from zope.schema.interfaces import InvalidValue
 from ZTUtils import make_query
 from zope.app.form.browser import OrderedMultiSelectWidget
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+try:
+    from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+except:
+    from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 from zope.app.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
 
